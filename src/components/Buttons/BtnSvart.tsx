d90079fdab0e9e36ab66b56bc70578d4b6753c66
asom1989 <asom.1989@yahoo.com>
2024-05-15 15:13:29 +0200

    lägg till buttons component

---
+++ src/components/Buttons/BtnSvart.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Button.css";
+
+interface BtnSvartProps {
+  onClick: () => void;
+  title: string;
+}
+
+const BtnSvart: React.FC<BtnSvartProps> = ({ title, onClick }) => {
+  return (
+    <button onClick={onClick} className="btn-svart flex">
+      {title}
+    </button>
+  );
+};
+
+export default BtnSvart;
