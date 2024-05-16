8a942c1696607e7a9e1327831b4115cb5c89fdc8
mariagryp <amnotyours@gmail.com>
2024-05-16 15:29:48 +0200

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
-import "./Footer/footer.css";
 import Header from "./components/Header/Header";
-import Footer from "./Footer/Footer";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
