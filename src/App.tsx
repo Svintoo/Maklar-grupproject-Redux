0784211bfcc3fdfd6e80f9131740885587273eee
mariagryp <amnotyours@gmail.com>
2024-05-16 11:26:59 +0200

    added footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./Footer/footer.css";
-import Header from "./components/header/Header";
+import Header from "./components/Header/Header";
 import Footer from "./Footer/Footer";
 
 function App() {
