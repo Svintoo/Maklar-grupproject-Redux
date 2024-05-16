bd85fe33e200f798efda8744e7826d7932e1779c
asom1989 <asom.1989@yahoo.com>
2024-05-16 11:41:20 +0200

    lagg till assets map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import "./Footer/footer.css";
+// import CardsWrapper from "./components/FastighetsCards/CardsWrapper";
+// import FastighetsCard from "./components/FastighetsCards/FastighetsCard";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
+      {/* <CardsWrapper>
+      <FastighetsCard/>
+    </CardsWrapper> */}
       <Footer />
     </>
   );
