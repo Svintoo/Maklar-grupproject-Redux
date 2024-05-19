66e36e498e5b8c54aa1507f4c20fc531b7eca2ec
asom1989 <asom.1989@yahoo.com>
2024-05-19 19:20:04 +0200

    några justeringar i header

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,19 +1,28 @@
 import "./Hero.css";
-import Image from "../../assets/imgs/hero_1280.jpg";
+import img3 from "../../assets/imgs/hans-m-q4Gmk6X_z7o-unsplash.jpg";
+import Header from "../header/Header";
 
 const Hero = () => {
   return (
-    <section
-      className="hero_container"
-      style={{ backgroundImage: `url(${Image})` }}
-    >
-      <div className="hero_content">
+    <div className="hero_container">
+      <Header />
+      <section
+        className="hero_container"
+        // style={{ backgroundImage: `url(${img1})` }}
+      >
+        <div className="hero_content">
+          <img src={img3} alt="" />
+          <p>
+            Vi hjälper dig att hitta ditt <em>drömhus</em>
+          </p>
+        </div>
+        {/* <div className="hero_content">
         <div className="hero_form_container">
           <h2 className="">Sök bostad:</h2>
           <form className="hero_form">
             <label>
               <input
-                type="text"
+                type="serch"
                 name="Find"
                 placeholder="Område, ort eller gata"
               />
@@ -22,9 +31,10 @@ const Hero = () => {
               Sök
             </button>
           </form>
-        </div>
-      </div>
-    </section>
+          </div>
+        </div> */}
+      </section>
+    </div>
   );
 };
 
